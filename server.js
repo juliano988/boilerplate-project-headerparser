8c12699167361025db483facdca9935c7022670b
juliano988 <julio-pinheiroo@hotmail.com>
2020-11-25 23:23:09 +0000

    Bug fixed on "ipaddress" property.

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/whoami",function(req,res){
-  res.json({ipaddress: req.headers.host, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+  res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
 })
 
 
