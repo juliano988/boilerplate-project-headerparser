320acbdecde2119a0c38a35467008c24c6836562
juliano988 <julio-pinheiroo@hotmail.com>
2020-11-25 14:40:22 -0300

    25/11/2020
    Final version

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // server.js
 // where your node app starts
 
+// To edit .env file
+require('dotenv').config();
+
 // init project
 var express = require('express');
 var app = express();
@@ -18,12 +21,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
+app.get("/api/whoami",function(req,res){
+  res.json({ipaddress: req.headers.host, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+})
 
 
 // listen for requests :)
